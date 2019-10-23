b8fd48295f004a784dac3a975ea06027f2014040
Ayush Bahuguna <ayushbahuguna8005@gmail.com>
2019-10-22 21:22:18 -0400

    consumer refactored

--- kafka-consumer-es/src/main/java/ElasticSearchConsumer.java
+++ kafka-consumer-es/src/main/java/ElasticSearchConsumer.java
@@ -4,6 +4,11 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -15,6 +20,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
 
 public class ElasticSearchConsumer {
 
@@ -40,20 +49,48 @@ public class ElasticSearchConsumer {
     public static void main(String[] args) throws IOException {
         Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
 
-
         RestHighLevelClient client = createClient();
 
-        String jsonString = "{ \"foo\": \"bar\" }";
+        KafkaConsumer<String, String> consumer = createConsumer();
 
-        IndexRequest request = new IndexRequest("twitter", "tweets").source(jsonString, XContentType.JSON);
-        IndexResponse response = client.index(request, RequestOptions.DEFAULT);
-        String id = response.getId();
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 
-        logger.info(id);
+            for (ConsumerRecord<String, String> record: records){
+                logger.info("Key: "+ record.key()+ ", Value: " + record.value());
+                logger.info("Partition: "+ record.partition()+ ", Offset: " + record.offset());
 
-        client.close();
+                IndexRequest request = new IndexRequest("twitter", "tweets").source(record.value(), XContentType.JSON);
+                IndexResponse response = client.index(request, RequestOptions.DEFAULT);
+                String id = response.getId();
 
+                logger.info(id);
 
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        //client.close();
+
+    }
+
+    public static KafkaConsumer<String, String> createConsumer() {
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, ElasticConfig.bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, ElasticConfig.groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Arrays.asList(ElasticConfig.topic));
+
+        return consumer;
     }
 
 }
